5d8abcbdca3e45d8457e3fccdbd1f52a7a50491d
Emad Saadat <234669292+saadatwxyz@users.noreply.github.com>
2025-11-12 14:54:36 -0800

    Move all parsing stuff to parser.py

--- interface.py
+++ interface.py
@@ -2,15 +2,6 @@
 import can
 import platform
 
-SENSOR_IDS = {
-    0x151: "S1_A", 0x152: "S1_B",
-    0x251: "S2_A", 0x252: "S2_B",
-    0x351: "S3_A", 0x352: "S3_B",
-    0x451: "S4_A", 0x452: "S4_B",
-    0x551: "S5_A", 0x552: "S5_B",
-    0x651: "S6_A", 0x652: "S6_B",
-}
-
 class Interface:
     def __init__(self):
         chan = ""
--- parser.py
+++ parser.py
@@ -1,40 +1,48 @@
-from interface import SENSOR_IDS
+ID_MAPPINGS = {
+    0x151: 1, 0x152: 1,
+    0x251: 2, 0x252: 2,
+    0x351: 3, 0x352: 3,
+    0x451: 4, 0x452: 4,
+    0x551: 5, 0x552: 5,
+    0x651: 6, 0x652: 6,
+}
 
-SENSOR_BUFFER = {
-  "S1": {"A": None, "B": None, "repackaged": None},
-  "S2": {"A": None, "B": None, "repackaged": None},
-  "S3": {"A": None, "B": None, "repackaged": None},
-  "S4": {"A": None, "B": None, "repackaged": None},
-  "S5": {"A": None, "B": None, "repackaged": None},
-  "S6": {"A": None, "B": None, "repackaged": None}
+SUBPACK_DATA = {
+  1: [0] * 12,
+  2: [0] * 12,
+  3: [0] * 12,
+  4: [0] * 12,
+  5: [0] * 12,
+  6: [0] * 12
 }
 
 def parse(msg):
-    if msg.arbitration_id in SENSOR_IDS:
-        # parse the message for it's sensor id and data
-        can_id = msg.arbitration_id
-        sensor_id = SENSOR_IDS.get(can_id, None)
-        
-        if sensor_id is None:
-            return None
-        
-        # split the sensor number and the part (A or B)
-        sensor_number, part = sensor_id.split("_")
+    msg_id = msg.arbitration_id
+    
+    if msg_id not in ID_MAPPINGS: return
+    
+    subpack_num = ID_MAPPINGS.get(msg_id, None)
+    
+    if sensor_id is None:
+        return None
+    
+    # split the sensor number and the part (A or B)
+    sensor_number, part = sensor_id.split("_")
 
-        # add the data to the buffer
-        SENSOR_BUFFER[sensor_number][part] = list(msg.data)
+    # add the data to the buffer
+    SENSOR_BUFFER[sensor_number][part] = list(msg.data)
 
-        a_side = SENSOR_BUFFER[sensor_number]["A"]
-        b_side = SENSOR_BUFFER[sensor_number]["B"]
+    a_side = SENSOR_BUFFER[sensor_number]["A"]
+    b_side = SENSOR_BUFFER[sensor_number]["B"]
 
-        # repackage the data into a single list
-        if a_side is not None and b_side is not None:
-            new_data = a_side + b_side
-            SENSOR_BUFFER[sensor_number]["repackaged"] = new_data # not sure if this is the best way to do this
+    # repackage the data into a single list
+    if a_side is not None and b_side is not None:
+        new_data = a_side + b_side
+        SENSOR_BUFFER[sensor_number]["repackaged"] = new_data # not sure if this is the best way to do this
 
-            print(f"Sensor: {sensor_number} | Data: {new_data}")
+        print(f"Sensor: {sensor_number} | Data: {new_data}")
 
-            # clear the buffer
-            SENSOR_BUFFER[sensor_number]["A"] = None
-            SENSOR_BUFFER[sensor_number]["B"] = None
+        # clear the buffer
+        SENSOR_BUFFER[sensor_number]["A"] = None
+        SENSOR_BUFFER[sensor_number]["B"] = None
 
